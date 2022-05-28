fb09994d7259dc9bba9ca4b823704a6fb27f3872
steve <103830370+net-js@users.noreply.github.com>
2022-05-28 22:58:29 +0530

    init

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   var [moreTime, setmoreTime] = useState("June 18")
   var [title, setTile] = useState("Blog-y")
   var props = {
-    dp:"Blogy a Website by net-js to blogy anything and everythong by net-js"
+    dp:"Project Olivia"
   }
   var countdownfunction = setInterval(function() {
 
@@ -26,31 +26,27 @@ export default function Home() {
       <title>{title}</title>
         <meta charset="UTF-8" />
         <meta name="description" content={props.dp} />
-        <meta name="keywords" content={`blogy,blogy-y,net-js`} />
+        <meta name="keywords" content={`oliva,Project-Olivia,net-js`} />
         <meta name="author" content="net-js" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="application-name" content="Blogy-y" />
-        <meta name="name" content="Blog-y" />
-
-        <link rel="manifest" href="/data.webmanifest" />
-        <link rel="icon" href="/icons/icon-48x48.png"/>
+        <meta name="application-name" content="Olivia" />
+        <meta name="name" content="Olivia" />
 
         <meta property="og:title" content={title} />
         <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
         <meta property="og:description" content={props.dp} />
-        <meta property="og:url" content="https://blog-y.vercel.app" />
-        <meta property="og:site_name" content="Blogy" />
-        <meta property="og:image" content="https://draw-snet.web.app/icons/icon-48x48.png" />
+        <meta property="og:url" content="https://olivia.vercel.com" />
+        <meta property="og:site_name" content="Olivia" />
         <meta name="twitter:card" content="summary" />
         <meta property="twitter:title" content={title} />
         <script type="application/ld+json">
           {JSON.stringify({
             "headline": title,
             "description": props.dp,
-            "url": "https://blog-y.vercel.app",
+            "url": "https://olivia.vercel.com",
             "@type": "WebSite",
-            "name": "Blogy",
+            "name": "Olivia",
             "@context": "https://schema.org"
           })}
         </script>
@@ -65,10 +61,10 @@ export default function Home() {
           <p style={{fontSize:"30px"}}>{moreTime}</p>
         </div>
         <div className="bottomleft">
-          <p>Blogy</p>
+          <p>Olivia</p>
         </div>
         <div className="bottomCenter">
-          <a href="https://github.com/net-js/net-js/discussions/2">Open Source.</a>
+          <a href="https://github.com/net-js/Project-Olivia/">Open Source.</a>
         </div>
       </div>
     </div>
